d2de844c06cc8ec3c0df5aca5a2ae7d4e758f81d
Valentina Aristizabal Escobar <valentina.aristizabale@upb.edu.co>
2025-09-30 17:00:37 -0500

    Se agregaron listas

---
+++ Listas/listas3.py
@@ -0,0 +1,18 @@
+# Datos de vuelo para un avión comercial
+tiempo =
+altitud =
+velocidad =
+estado = ["despegue", "ascenso inicial", "ascenso", ]
+
+# Imprimir informe de despegue
+
+
+
+
+
+suma = 0
+for v in velocidad:
+    suma += v
+
+promedio = suma / len(velocidad)
+print
